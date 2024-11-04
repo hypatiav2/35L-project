1440889d29c0c9116e40199214438fd1f0310cb2
apaphy <82987703+apaphy@users.noreply.github.com>
2024-11-03 21:32:13 -0800

    installed tailwind

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoginPage from './login/page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (false) // if logged in
+{
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
+else {
+    root.render(
+        <React.StrictMode>
+            <LoginPage />
+        </React.StrictMode>
+    );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
