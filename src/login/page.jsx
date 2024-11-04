86fa566e412fd6a10095295d4956ee7ea331c818
apaphy <82987703+apaphy@users.noreply.github.com>
2024-11-03 22:13:18 -0800

    login working fr fr

--- src/login/page.jsx
+++ src/login/page.jsx
@@ -71,19 +71,21 @@ export default function LoginPage()
                         </div>
                     )
                     : (
-                        <form className="flex flex-col items-center space-y-6" onSubmit={handleLogin}>
+                        <div className="flex flex-col items-center space-y-6">
                             <div className="flex flex-col space-y-2 w-[300px]">
                                 <label className="input input-bordered bg-white flex items-center gap-2 shadow-sm">                        
-                                    <input name="email" type="text" className="grow text-sm text-neutral-700 selection:bg-neutral-200" placeholder="Email" />
+                                    <input name="email" type="text" className="grow text-sm text-neutral-700 selection:bg-neutral-200" placeholder="Email" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}/>
                                 </label>
                                 <label className="input input-bordered bg-white flex items-center gap-2 shadow-sm">
-                                    <input name="password" type="password" placeholder="Password" className="grow text-sm text-neutral-700 selection:bg-neutral-200" />
+                                    <input name="password" type="password" placeholder="Password" className="grow text-sm text-neutral-700 selection:bg-neutral-200" onChange={(e) => setPassword(e.target.value)} value={password}/>
                                 </label>
                             </div>
-                            <button className="btn btn-sm shadow-sm px-8 h-[40px] rounded-full bg-blue-700 border-none text-white hover:bg-blue-300" type="submit">
+                            <button className="btn btn-sm shadow-sm px-8 h-[40px] rounded-full bg-blue-700 border-none text-white hover:bg-blue-300" onClick={handleLogin}>
                                 Log In
                             </button>
-                        </form>
+                        </div>
                     )
             }
         </div>
