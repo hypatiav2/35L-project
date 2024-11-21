ca2c5ee93128c80dcaed715df41930b43940b113
t1ffanyc <tchen1515@gmail.com>
2024-11-20 17:36:46 -0800

    finished quiz frontend, added to navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { useEffect } from 'react';
 import LoginPage from './login/page';
 import HomePage from './home/page';
 import ProfilePage from './profilepage/page';
-import WelcomePage from './welcome/page'
+import WelcomePage from './welcome/page';
+import QuizPage from './quiz/page';
 import ProtectedRoute from './ProtectedRoute';
 import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './AuthContext';
@@ -23,17 +24,18 @@ function App() {
 
     return (
         <Routes>
-            <Route path="/welcome" element={<WelcomePage />} />            
-            <Route path="/login" element={<LoginPage />} />
+            <Route path='/welcome' element={<WelcomePage />} />            
+            <Route path='/login' element={<LoginPage />} />
             <Route
-                path="/home"
+                path='/home'
                 element={
                     <ProtectedRoute>
                         <HomePage />
                     </ProtectedRoute>
                 }
             />
-            <Route path='/profile' element={<ProfilePage/>} />
+            <Route path='/profile' element={<ProfilePage />} />
+            <Route path='/quiz' element={<QuizPage />} />
             <Route path="*" element={<Navigate to={isAuthenticated ? '/home' : '/login'} replace />} />
         </Routes>
     );
