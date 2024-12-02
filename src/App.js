13277abb479fd77949b72cdb413d15a981b06629
hypatiav2 <73620120+hypatiav2@users.noreply.github.com>
2024-12-01 21:22:22 -0800

    Revert "remove all auto redirects"
    This reverts commit ec417fe48d3361202e342d4c25b78e4e48af0c53.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import QuizPage from './quiz/page';
 import ProtectedRoute from './ProtectedRoute';
 import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './AuthContext';
+import SchedulePage from './schedule/page';
 
 function App() {
     const { isAuthenticated } = useAuth();
     const navigate = useNavigate();
+
 
     return (
         <Routes>
@@ -29,6 +31,7 @@ function App() {
             <Route path='/viewprofiles' element={<ViewProfilesPage />} />
             <Route path='/profile' element={<ProfilePage />} />
             <Route path='/quiz' element={<QuizPage />} />
+            <Route path='/schedule' element={<SchedulePage />} />
             <Route path="*" element={<Navigate to={isAuthenticated ? '/home' : '/login'} replace />} />
         </Routes>
     );
