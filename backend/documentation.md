300008b5fc437b02e7be9ce3f2e77c2662f0d836
dmychen <daniel@chenhome.org>
2024-12-03 01:12:30 -0800

    update matches logic and fix things
    1. move count and offset into request params not request body for GET matches.
    2. GET matches automatically update matches table when appropriate
    3. GET matches properly returns a subset of matches based on offset and count.
    4. Match objects store a particular availability timeslot.
    5. Get matches returns individual Match objects for each overlapping timeslot, potentially multiple per other user.
    6. GetAllAvailable() returns overlapping availability between users, not just userIds
    7. rewrite ComputeMatches() to work properly
    8. fix breaking bug in updateMatches() (accessing outside an array)

--- backend/documentation.md
+++ backend/documentation.md
@@ -145,27 +145,33 @@ Returns:
 
 ## Matches
 
-**`GET /api/v1/matches`**: find the top matches for a user. (NOT FULLY IMPLEMENTED YET)
+**`GET /api/v1/matches`**: find the top matches for a user.
 
-- Finds other users who have overlapping availability with the current user
-- returns as a list "matches", with the other user's id and a similarity score between the two users
+Returns a list of Match objects: each match corresponds to a overlapping availability between the current user and another user.
+The list is sorted by the similarity score between [current user] and [other user].
 
-Request Body:
-	{
-		"count": <the number of matches to return, default 10> INT
-		"offset": <The starting point for the results, default 0> INT
-	}
-	
-> Example: with count = 20 and offset = 10, this GET will return the 10th to 30th most similar users to the current user, who have overlapping availability.
+Request Params:
+
+	count: number of matches to return
+	offset: offset from beginning of matches list to return from
+
+> Example:
+> count = 20 and offset = 10,
+> `GET /api/v1/matches?count=20&offset=10` returns the 10th to 30th overlapping availability to the current user, sorted by similarity.
 
 Returns:
+
 	200 OK: Returns a list of matches
+	[
 		{
+
 			"user1_id": <current user> STRING
 			"user2_id": <matched user> STRING
 			"similarity_score": <similarity between user 1 and 2> FLOAT,
 	    	"match_status": <if the match has been accepted> TEXT or NULL ('pending', 'accepted', 'rejected', null)
 		}
+	]
+
 
 ## Users
 
--- backend/models/matches.go
+++ backend/models/matches.go
@@ -3,6 +3,7 @@ package models
 import (
 	"database/sql"
 	"fmt"
+	"sort"
 )
 
 const BATCH_SIZE = 50
@@ -11,6 +12,9 @@ type Match struct {
 	ID         int     `json:"id"`
 	User1ID    string  `json:"user1_id"`
 	User2ID    string  `json:"user2_id"`
+	DayOfWeek  string  `json:"day_of_week"`
+	StartTime  string  `json:"start_time"`
+	EndTime    string  `json:"end_time"`
 	Similarity float64 `json:"similarity_score"`
 	Status     *string `json:"match_status"`
 }
@@ -19,22 +23,72 @@ type Match struct {
 func ComputeMatches(userID string, db *sql.DB) ([]Match, error) {
 
 	// Step 1: Find users with overlapping availabilities
-	matches, err := GetAllAvailable(userID, db)
+	userAvailabilities, err := GetAllAvailable(userID, db)
 	if err != nil {
 		fmt.Printf("Error finding users with overlapping availabilities: %v\n", err)
 		return nil, err
 	}
 
+	// extract users
+	var users []string
+	for key := range userAvailabilities {
+		users = append(users, key)
+	}
+
 	// Step 2: Compute similarities
-	rankedMatches, err := ComputeSimilarity(matches, userID, db)
+	similarityScores, err := ComputeSimilarity(users, userID, db)
 	if err != nil {
 		fmt.Printf("Error computing similarities: %v\n", err)
 		return nil, err
 	}
 
+	// Sort similarities by similarity score
+	sort.Slice(similarityScores, func(i, j int) bool {
+		return similarityScores[i].Score > similarityScores[j].Score // Sort in descending order
+	})
+
+	// create Match objects for each availability timeslot
+	matches, err := createMatches(userID, similarityScores, userAvailabilities)
+	if err != nil {
+		fmt.Println("Error creating matches:", err)
+		return nil, err
+	}
 	// Return list of match objects
 
-	return rankedMatches, nil
+	return matches, nil
+}
+
+// Given a base user, and a set of similarities and availabilities, create a list of Match objects between the base user and each availability in availabilities.
+func createMatches(baseUser string, sortedSimilarities []Similarity, availabilities map[string][]Availability) ([]Match, error) {
+	var matches []Match
+
+	// Iterate over the sorted similarities
+	for _, Similarity := range sortedSimilarities {
+		userId := Similarity.UserID
+		// Get the list of availabilities for each user
+		usersAvailabilities, exists := availabilities[userId]
+		if !exists {
+			continue // skip user if no availabilities are found
+		}
+
+		// For each availability for this user, create a Match object
+		for _, availability := range usersAvailabilities {
+			match := Match{
+				User1ID:    baseUser,
+				User2ID:    availability.UserID,
+				DayOfWeek:  availability.DayOfWeek,
+				StartTime:  availability.StartTime,
+				EndTime:    availability.EndTime,
+				Similarity: Similarity.Score,
+				Status:     nil,
+			}
+
+			// Append to matches list
+			matches = append(matches, match)
+		}
+	}
+
+	return matches, nil
 }
 
 // Update matches table, which stores the top BATCH_SIZE most similar matches for any given user
@@ -58,7 +112,8 @@ func UpdateMatches(userID string, db *sql.DB) error {
 	defer stmt.Close()
 
 	// Take the top 50 matches and insert into matches table
-	for i := 0; i < BATCH_SIZE; i++ {
+	batch := min(len(computedMatches), BATCH_SIZE)
+	for i := 0; i < batch; i++ {
 		currentMatch := computedMatches[i]
 
 		_, err = stmt.Exec(currentMatch.User1ID, currentMatch.User2ID, currentMatch.Similarity, currentMatch.Status)
