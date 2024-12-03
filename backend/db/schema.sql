8773b521221a92e0fdc81c9979394cc056891ad6
dmychen <daniel@chenhome.org>
2024-12-03 03:04:44 -0800

    reimplement dates endpoints
    1. remove status from matches table
    2. add status and user ids into dates table, remove match ids.
    3. implement GET dates to have an optional status param, and return dates for the current user that have that status (or all on default)
    4. store DateStart/DateEnd as strings instead of time.Times (too much of a headache)
    5. implement POST dates to take a userID (other user) and date start/end times
    6. implement PATCH dates, which changes the status of a date

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -22,16 +22,17 @@ CREATE TABLE matches (
     user1_id TEXT NOT NULL,
     user2_id TEXT NOT NULL,
     similarity_score REAL,
-    match_status TEXT DEFAULT NULL, -- 'pending', 'accepted', 'rejected', NULL if haven't matched yet
     FOREIGN KEY(user1_id) REFERENCES users(id),
     FOREIGN KEY(user2_id) REFERENCES users(id)
 );
 
 CREATE TABLE scheduled_dates (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    match_id INTEGER NOT NULL,
+    user1_id TEXT NOT NULL,
+    user2_id TEXT NOT NULL,
     date_start TEXT NOT NULL, -- we want this in ISO 8601 format
     date_end TEXT NOT NULL,   -- same here
-    is_confirmed BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY(match_id) REFERENCES matches(id)
+    status TEXT DEFAULT "pending", -- "pending", "confirmed", "rejected"
+    FOREIGN KEY(user1_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(user2_id) REFERENCES users(id) ON DELETE CASCADE
 );
