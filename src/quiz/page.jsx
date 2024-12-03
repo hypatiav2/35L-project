145cfa5c7d1b6ec9e2e67b7ad663d7ab8004feaf
t1ffanyc <tchen1515@gmail.com>
2024-12-02 23:32:34 -0800

    improve quiz ui

--- src/quiz/page.jsx
+++ src/quiz/page.jsx
@@ -9,16 +9,16 @@ export default function QuizPage() {
  const { isAuthenticated, getSupabaseClient } = useAuth();
 
  const contents=[
-   ["How much do you talk?", "1 - Mute", "5 - Fluent in Yapanese"],
-   ["How much do you like your alone time?", "1 - Always with people", "5 - Always alone"],
-   ["How much do you like/listen to music?", "1 - What is a song", "5 - Cannot live wihtout music"],
-   ["How much do you like/play sports?", "1 - I don't go outside", "5 - D1 athlete"],
-   ["Do you enjoy partying/drinking?", "1 - Never", "5 - Wilding every night"],
-   ["How chill are you?", "1 - Not so easygoing", "5 - Easygoing"],
-   ["STEM or art?", "1 - Art", "5 - STEM"],
-   ["How would you describe your life currenlty?", "1 - Absolute clownery", "5 - Have life together"],
-   ["How fast is your response time?", "1 - Several days", "5 - Immediately"],
-   ["What do you enjoy doing?", "1 - Watching a movie in bed", "5 - Adventuring in the Alps"]
+   ["How much do you talk?", "Mute", "Fluent in Yapanese"],
+   ["How much do you like your alone time?", "Always with people", "Always alone"],
+   ["How much do you like/listen to music?", "What is a song", "Can't live without music"],
+   ["How much do you like/play sports?", "I don't go outside", "D1 athlete"],
+   ["Do you enjoy partying/drinking?", "Never", "Wilding every night"],
+   ["How chill are you?", "Not so easygoing", "Easygoing"],
+   ["STEM or art?", "Art", "STEM"],
+   ["How would you describe your life currenlty?", "Absolute clownery", "Have life together"],
+   ["How fast is your response time?", "Several days", "Immediately"],
+   ["What do you enjoy doing?", "Watching a movie in bed", "Adventuring in the Alps"]
   ]
 
   const [scores, setScores] = useState(Array(contents.length).fill(null))
@@ -74,15 +74,15 @@ export default function QuizPage() {
       alert('Unexpected error occurred. Check the console for details.');
     }
 
-
     navigate('/home');
   };
 
   return (
    <div>
      <Navbar />
-     <div className="min-h-screen bg-gray-100 py-8 px-4">
-       <h1 className="text-3xl font-bold text-center mb-6 text-gray-700 max-w-2xl mx-auto bg-white p-8 rounded-lg shadow-lg">Compatibility Quiz</h1>
+     <div className="min-h-screen py-8 px-4">
+       <h1 className="text-2xl font-bold text-center mb-6 text-gray-800 max-w-2xl mx-auto p-4">Compatibility Quiz</h1>
+       <h1 className="text-lg text-gray-600 mb-8 text-center max-w-3xl mx-auto">Our quiz has ten questions designed to assess your values and strengths. We use the results to match you with people whose results suggest a high compatibility score with you. </h1>
        <form onSubmit={handleSubmit} className="space-y-6">
          <Question name={0} contents={contents[0]} onChange={handleScoreChange}/>
          <Question name={1} contents={contents[1]} onChange={handleScoreChange}/>
@@ -94,10 +94,10 @@ export default function QuizPage() {
          <Question name={7} contents={contents[7]} onChange={handleScoreChange}/>
          <Question name={8} contents={contents[8]} onChange={handleScoreChange}/>
          <Question name={9} contents={contents[9]} onChange={handleScoreChange}/>
-         <div className="flex justify-center">
+         <div className="flex justify-center py-8">
              <button
                  type="submit"
-                 className="px-6 py-3 bg-indigo-600 text-white font-semibold rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                 className="text-blue-600 font-semibold border border-blue-600 px-4 py-2 hover:text-white hover:bg-blue-600 rounded transition"
              >
                  Submit
              </button>
@@ -108,30 +108,43 @@ export default function QuizPage() {
  );
 };
 
-
 function Question({ name, contents, onChange }) {
- return (
-   <div className="max-w-2xl mx-auto bg-white p-8 rounded-lg shadow-lg">
-     <h2 className="text-gray-600 font-medium mb-2">{contents[0]}</h2>
-     <ul class="choices">
-       <Option name={name} value={1} message={contents[1]} onChange={onChange}/>
-       <Option name={name} value={2} message="2" onChange={onChange}/>
-       <Option name={name} value={3} message="3" onChange={onChange}/>
-       <Option name={name} value={4} message="4" onChange={onChange}/>
-       <Option name={name} value={5} message={contents[2]} onChange={onChange}/>
-     </ul>
-   </div>
- );
-};
-
-
-function Option({ name, value, message, onChange }) {
- return (
-   <li>
-     <label>
-       <input type="radio" name={name} value={value} required onChange={() => onChange(name, value)}></input>{message}
-     </label>
-   </li>
- )
+  return (
+    <div className="max-w-4xl mx-auto p-8 border-t-2">
+      <h2 className="text-blue-600 text-lg font-semibold text-center mb-10">{contents[0]}</h2>
+      <div className="flex items-center">
+        <span className="basis-1/6 text-gray-500 font-medium text-center">{contents[1]}</span>
+        <ul className="basis-2/3 flex space-x-4 items-center justify-center">
+          <Option name={name} value={1} onChange={onChange} />
+          <Option name={name} value={2} onChange={onChange} />
+          <Option name={name} value={3} onChange={onChange} />
+          <Option name={name} value={4} onChange={onChange} />
+          <Option name={name} value={5} onChange={onChange} />
+        </ul>
+        <span className="basis-1/6 text-gray-500 font-medium text-center">{contents[2]}</span>
+      </div>
+    </div>
+  );
 }
 
+function Option({ name, value, onChange }) {
+  return (
+    <li className="relative">
+      <label className="flex items-center justify-center w-16 h-12 rounded-full cursor-pointer">
+        <input
+          type="radio"
+          name={name}
+          value={value}
+          required
+          className="sr-only peer"
+          onChange={() => onChange(name, value)}
+        />
+        <span
+          className="block w-12 h-12 border-2 border-gray-300 rounded-full 
+          peer-hover:border-blue-600 peer-checked:border-blue-600 
+          peer-checked:bg-blue-100"
+        ></span>
+      </label>
+    </li>
+  );
+}
